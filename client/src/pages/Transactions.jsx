92768af438e325e68f7f84c3d10f85f44fdd7034
Vaishnavi Pande <122804106+vaishpande27@users.noreply.github.com>
2025-08-20 15:43:34 +0530

    Update Transactions.jsx

--- client/src/pages/Transactions.jsx
+++ client/src/pages/Transactions.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+
 
 function Transactions() {
   const { groupId } = useParams();
