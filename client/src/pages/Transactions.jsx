c61100a872f0c98da4e6684590835f6d017008aa
Vaishnavi Pande <122804106+vaishpande27@users.noreply.github.com>
2025-08-20 15:43:51 +0530

    Update Transactions.jsx

--- client/src/pages/Transactions.jsx
+++ client/src/pages/Transactions.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import Calendar from "react-calendar";
 
 
 function Transactions() {
